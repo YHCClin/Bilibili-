66cc52e5bd74abde63c91d7ded4a8288535f9761
YHCClin <lyhdxzb@icloud.com>
2020-02-04 12:51:29 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,139 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 首页导航数据
+    navList:[],
+    // 被点击首页导航的菜单的索引
+    currentIndexNav:0,
+    // 轮播图数据
+    swiperList:[],
+    // 视频列表数据
+    videosList:[]
+  },
+
+  // 点击首页导航按钮时触发
+  activeNav(e) {
+    //console.log(123);
+    this.setData({
+      currentIndexNav:e.target.dataset.index // 获取 data-index 的值
+    })
+  },
+  /**
+   * 获取视频列表
+   */
+  getVideosList() {
+    let that = this;
+    wx.request({
+      url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videosList',
+      success(res){
+        console.log(res);
+        if(res.data.code===0){
+          that.setData({
+            videosList:res.data.data.videosList
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取轮播图数据
+   */
+  getSwiperList() {
+    let that = this;
+    wx.request({
+      url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/swiperList',
+      success(res){
+        console.log(res);
+        if(res.data.code===0) {
+          that.setData({
+            swiperList:res.data.data.swiperList
+          })
+        }
+      }
+    })
+  },
+  /*
+    获取首页的导航数据
+   */
+  getNavList() {
+    // that 为辅助变量
+    let that = this;
+    // 利用小程序内置的发送请求的方法
+    wx.request({
+      url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/navList",
+      // 如果成功获取数据 if success
+      success(res){
+        console.log(res);
+        if(res.data.code === 0) {
+          that.setData({
+            navList:res.data.data.navList
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //1  获取首页导航数据
+    this.getNavList();
+    //2 获取轮播图数据
+    this.getSwiperList();
+    //3 获取视频列表数据
+    this.getVideosList();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
